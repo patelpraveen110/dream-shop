c8f8b58622e078279a597bde7939e299d2989e8d
patelpraveen110 <patelpraveen110@gmail.com>
2024-09-29 16:16:58 +0530

    first commit

---
+++ src/main/java/com/codeshop/DreamShopCartApplication.java
@@ -0,0 +1,13 @@
+package com.codeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamShopCartApplication {
+
+    public static void main(String[] args) {
+		SpringApplication.run(DreamShopCartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeshop/DreamShopCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamShopCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
